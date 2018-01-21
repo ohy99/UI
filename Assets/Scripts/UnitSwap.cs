e8148821e4337c774086bd02db74d919f3a79d23
ohy99 <ohy99@hotmail.com>
2018-01-21 21:01:04 +0800

    MainMenu semi done

---
+++ Assets/Scripts/UnitSwap.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UnitSwap : MonoBehaviour {
+
+    Button sender = null;
+    [SerializeField]
+    GameObject overlayPanel;
+
+	// Use this for initialization
+	void Start () {
+        overlayPanel.active = false;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ToggleOverlayCallback(Button OnClickSender)
+    {
+        overlayPanel.active = !overlayPanel.active;
+        Debug.Log(overlayPanel.active);
+        if (overlayPanel.active)
+        {
+            //store the sender;
+            this.sender = OnClickSender;
+        }
+        else
+        {
+            //offing the overlay
+            if(OnClickSender.name.Substring(0, 4).Equals("Unit"))
+            {
+                this.sender.image.sprite = OnClickSender.image.sprite;
+            }
+            this.sender = null;
+        }
+        Debug.Log(sender);
+    }
+}
