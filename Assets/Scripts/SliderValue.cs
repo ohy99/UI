163516e7ae5b553ac07fdeee1599c568e4c57352
ohy99 <ohy99@hotmail.com>
2018-01-22 02:16:18 +0800

    Social and Options done

---
+++ Assets/Scripts/SliderValue.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderValue : MonoBehaviour {
+
+    [SerializeField]
+    Slider slider;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnValueChangedCallBack(Text textbox)
+    {
+        textbox.text = (slider.value * 100).ToString("0");
+    }
+}
