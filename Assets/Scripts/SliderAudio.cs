cd78d19fc4c505a25d900bc8e6bea551204c6591
ohy99 <ohy99@hotmail.com>
2018-01-23 01:26:21 +0800

    option slider onenddrag play sound

---
+++ Assets/Scripts/SliderAudio.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderAudio : MonoBehaviour {
+
+    [SerializeField]
+    Slider slider;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnEndDrag(AudioSource audio)
+    {
+        audio.volume = slider.value;
+        audio.Play();
+    }
+}
